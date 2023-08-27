55ecf799c81919c9de7c72d3c6bcfbf0b908e255
Pedro Henrique <ph.laurindo2017@gmail.com>
2023-08-27 15:58:33 -0300

    Resolução concluída

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,31 @@
 package application;
 
-public class Program {
+import java.util.Scanner;
 
+import model.ContaTerminal;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Entre com as informações da conta:");
+		System.out.print("Informe o número da conta: ");
+		int numeroConta = sc.nextInt();
+		System.out.print("Informe o número da agencia: ");
+		String numeroAgencia = sc.next();
+		System.out.print("Informe o nome do cliente: ");
+		sc.nextLine();
+		String nomeCliente = sc.nextLine();
+		System.out.print("Informe o saldo da conta: ");
+		double saldoConta = sc.nextDouble();
+		
+		ContaTerminal conta = new ContaTerminal(numeroConta, numeroAgencia, nomeCliente, saldoConta);
+		
+		System.out.println("Olá "+ conta.getNomeCliente() +", obrigado por criar uma conta em nosso banco, sua agência é "+ conta.getAgencia() +",\n conta " + conta.getNumero() +" e seu saldo "+ conta.getSaldo() +" já está disponível para saque.");
+		
+		
+		sc.close();
+	}
 }
--- src/model/ContaTerminal.java
+++ src/model/ContaTerminal.java
@@ -1,5 +1,52 @@
 package model;
 
 public class ContaTerminal {
+	
+	private int numero;
+	private String agencia;
+	private String nomeCliente;
+	private double saldo;
+	
+	public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
+		super();
+		this.numero = numero;
+		this.agencia = agencia;
+		this.nomeCliente = nomeCliente;
+		this.saldo = saldo;
+	}
 
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
